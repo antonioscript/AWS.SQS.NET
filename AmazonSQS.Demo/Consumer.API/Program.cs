385001aca15b820f5590a6f65f64a54b665df2f3
Antônio Rocha <antoniojunior159@gmail.com>
2025-06-03 23:05:07 -0300

    Configure Consumer

--- AmazonSQS.Demo/Consumer.API/Program.cs
+++ AmazonSQS.Demo/Consumer.API/Program.cs
@@ -1,3 +1,6 @@
+using Amazon.SQS;
+using Consumer.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Consumer
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<IAmazonSQS>();
+builder.Services.AddHostedService<OrderCreatedEventConsumer>();
 
 var app = builder.Build();
 
